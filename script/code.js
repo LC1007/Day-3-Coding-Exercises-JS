58ab2460a8a5fc2bed79b2d96b0fe3172c64741f
LC1007 <tonyg7212@gmail.com>
2023-05-17 09:56:32 +0200

    Day 3 - Coding Exercise

---
+++ script/code.js
@@ -0,0 +1,39 @@
+// JS Data Type Exercise
+
+// 1. Make a variable with a data of a string and check the type of this variable.
+
+let firstName = "Justin"
+console.log(typeof firstName);
+
+// 2. Reassign the value to a number and check the type again. Did it change?
+
+firstName = 24
+console.log(typeof firstName);
+
+// 3. Go ahead and create a variable with every data type you know of. Double check that you covered all of them by using the typeof check.
+
+let fullName = "John Doe"
+console.log(typeof fullName);
+
+let salary = 15000
+console.log(typeof salary);
+
+let jobAccepted = true
+console.log(typeof jobAccepted);
+
+let something = undefined
+console.log(typeof something);
+
+let empty = null
+console.log(typeof empty);
+
+let list = ["Microsoft","Mac","Windows","Laptop","Desktop"]
+console.log(typeof list);
+
+let personDetails = [{
+    firstName: "John",
+    lastName: "Doe",
+    fullName: "John Doe",
+    age: 30
+}]
+console.log(typeof personDetails);
